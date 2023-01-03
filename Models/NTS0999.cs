2eaa89d9fffaf5cb0e94a552cb422fe16047524d
Nguyen Tung Son <titlai434321@gmail.com>
2023-01-03 18:23:45 +0700

    Final_Commit_NTS999

--- Models/NTS0999.cs
+++ Models/NTS0999.cs
@@ -2,7 +2,8 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Test6_FinalTest.Models;
 
-namespace Test6_FinalTest.Models;
+namespace Test6_FinalTest.Models
+{
 [Table("NTS0999")]
 public class NTS0999
 {
@@ -10,6 +11,9 @@ public class NTS0999
     public string? NTSId {get; set;}
     [Required, StringLength(50), Display(Name = "Tên")]
     public string? NTSName {get; set;}
+    [Required, Display(Name ="Giới tính nam")]
     public Boolean NTSGender {get; set;}
 }
+}
+
  
---
+++ Models/StringProcessNTS999.cs
@@ -0,0 +1,10 @@
+namespace Test6_FinalTest.Models
+{
+    public class StringProcessNTS999
+    {
+        public string? LowerToUpper (string strInput)
+        {
+            return strInput.ToUpper();
+        }
+    }
+}
