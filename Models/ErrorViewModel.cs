26a86c75a45bdc1c14deb35e165ac4f38fd609a0
Nguyen Tung Son <titlai434321@gmail.com>
2023-01-03 17:14:31 +0700

    first commit Test6_FinalTest

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Test6_FinalTest.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
